0d94d4dfbc1a4fae6ae0ed5dad5b58c15e013e20
johandry loyo <Johanloyo6@gmail.com>
2022-06-03 10:59:04 -0400

    law view complete

--- src/pages/Law/index.tsx
+++ src/pages/Law/index.tsx
@@ -16,11 +16,19 @@ const index = (props: Props) => {
 	const [openModal3, setOpen3] = useState<boolean>(false);
 	const [openModal4, setOpen4] = useState<boolean>(false);
 	const [openModal5, setOpen5] = useState<boolean>(false);
+	const [openModal6, setOpen6] = useState<boolean>(false);
+	const [openModal7, setOpen7] = useState<boolean>(false);
+	const [openModal8, setOpen8] = useState<boolean>(false);
+	const [openModal9, setOpen9] = useState<boolean>(false);
 	const handleOpen1 = () => (openModal1 ? setOpen1(false) : setOpen1(true));
 	const handleOpen2 = () => (openModal2 ? setOpen2(false) : setOpen2(true));
 	const handleOpen3 = () => (openModal3 ? setOpen3(false) : setOpen3(true));
 	const handleOpen4 = () => (openModal4 ? setOpen4(false) : setOpen4(true));
 	const handleOpen5 = () => (openModal5 ? setOpen5(false) : setOpen5(true));
+	const handleOpen6 = () => (openModal6 ? setOpen6(false) : setOpen6(true));
+	const handleOpen7 = () => (openModal7 ? setOpen7(false) : setOpen7(true));
+	const handleOpen8 = () => (openModal8 ? setOpen8(false) : setOpen8(true));
+	const handleOpen9 = () => (openModal9 ? setOpen9(false) : setOpen9(true));
 
 	return (
 		<div>
@@ -68,7 +76,7 @@ const index = (props: Props) => {
 
 				<div className={styles['acordion-item']}>
 					<div className={styles['acordion-head']} onClick={handleOpen2}>
-						<span className={styles['icon']}>
+						<span className={openModal2 ? styles['rotate'] : styles['icon']}>
 							<FaGreaterThan />
 						</span>
 						<div className={styles['acordion-title']}>Correo de bienvenida</div>
@@ -82,7 +90,7 @@ const index = (props: Props) => {
 
 				<div className={styles['acordion-item']}>
 					<div className={styles['acordion-head']} onClick={handleOpen3}>
-						<span className={styles['icon']}>
+						<span className={openModal3 ? styles['rotate'] : styles['icon']}>
 							<FaGreaterThan />
 						</span>
 						<div className={styles['acordion-title']}>
@@ -98,12 +106,81 @@ const index = (props: Props) => {
 
 				<div className={styles['acordion-item']}>
 					<div className={styles['acordion-head']} onClick={handleOpen4}>
-						<span className={styles['icon']}>
+						<span className={openModal4 ? styles['rotate'] : styles['icon']}>
 							<FaGreaterThan />
 						</span>
 						<div className={styles['acordion-title']}>Estudio de titulo</div>
 					</div>
 					<div className={!openModal4 ? styles['hidden'] : ''}>
+						<div className={styles['acordion-contenedor']}>
+							<Table></Table>
+						</div>
+					</div>
+				</div>
+
+				<div className={styles['acordion-item']}>
+					<div className={styles['acordion-head']} onClick={handleOpen5}>
+						<span className={openModal5 ? styles['rotate'] : styles['icon']}>
+							<FaGreaterThan />
+						</span>
+						<div className={styles['acordion-title']}>Etapa compra venta</div>
+					</div>
+					<div className={!openModal5 ? styles['hidden'] : ''}>
+						<div className={styles['acordion-contenedor']}>
+							<Table></Table>
+						</div>
+					</div>
+				</div>
+
+				<div className={styles['acordion-item']}>
+					<div className={styles['acordion-head']} onClick={handleOpen6}>
+						<span className={openModal6 ? styles['rotate'] : styles['icon']}>
+							<FaGreaterThan />
+						</span>
+						<div className={styles['acordion-title']}>Etapa compra venta</div>
+					</div>
+					<div className={!openModal6 ? styles['hidden'] : ''}>
+						<div className={styles['acordion-contenedor']}>
+							<Table></Table>
+						</div>
+					</div>
+				</div>
+				<div className={styles['acordion-item']}>
+					<div className={styles['acordion-head']} onClick={handleOpen7}>
+						<span className={openModal7 ? styles['rotate'] : styles['icon']}>
+							<FaGreaterThan />
+						</span>
+						<div className={styles['acordion-title']}>
+							Etapa inscripción CBR
+						</div>
+					</div>
+					<div className={!openModal7 ? styles['hidden'] : ''}>
+						<div className={styles['acordion-contenedor']}>
+							<Table></Table>
+						</div>
+					</div>
+				</div>
+				<div className={styles['acordion-item']}>
+					<div className={styles['acordion-head']} onClick={handleOpen8}>
+						<span className={openModal8 ? styles['rotate'] : styles['icon']}>
+							<FaGreaterThan />
+						</span>
+						<div className={styles['acordion-title']}>Etapa de pagos</div>
+					</div>
+					<div className={!openModal8 ? styles['hidden'] : ''}>
+						<div className={styles['acordion-contenedor']}>
+							<Table></Table>
+						</div>
+					</div>
+				</div>
+				<div className={styles['acordion-item']}>
+					<div className={styles['acordion-head']} onClick={handleOpen9}>
+						<span className={openModal9 ? styles['rotate'] : styles['icon']}>
+							<FaGreaterThan />
+						</span>
+						<div className={styles['acordion-title']}>Entrega de propiedad</div>
+					</div>
+					<div className={!openModal9 ? styles['hidden'] : ''}>
 						<div className={styles['acordion-contenedor']}>
 							<Table></Table>
 						</div>
